4937da878dd3420dd5aec0c2e4cb8d9ef1fb8c91
kimsegong <kimsegong@naver.com>
2023-10-03 14:32:20 +0900

    20231003

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
